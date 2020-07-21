c242a209ba22f2b9259718469d06ad1933e1c079
wenhao <1243719609@qq.com>
2020-07-21 15:50:15 +0800

    Initial commit

---
+++ src/main/test/ServiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
+public class ServiceTest {
+    @Autowired
+    UserService userService;
+    @Test
+    public void userServiceTest(){
+        User user=userService.findUserByUserName("admin");
+        System.out.println(user.getPassword());
+    }
+}
