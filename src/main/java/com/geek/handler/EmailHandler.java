2991702c209f9533bd393d7e909eb50aa29ce733
QCnice <963950539@qq.com>
2020-07-28 15:09:01 +0800

    Merge branch 'master' of https://github.com/wenhao19971211/OASystem
    # Conflicts:
    #	src/main/resources/mapper/mapper-yearLeave.xml
    #	src/main/resources/mybatis.xml
    #	src/main/webapp/page/index.html
    2020/7/27

--- src/main/java/com/geek/handler/EmailHandler.java
+++ src/main/java/com/geek/handler/EmailHandler.java
@@ -45,11 +45,15 @@ public class EmailHandler {
      */
     @PostMapping("sendEmail")
     public Integer sendEmail(int empId, int type, String title, String content,int [] receive){
-//        System.out.println(receive.length);
-//        System.out.println("发送人编号"+empId);
-//        System.out.println("发送的类别"+type);
-//        System.out.println("发送人标题"+title);
-//        System.out.println("发送人内容"+content);
+        System.out.println("进入了发送邮件");
+        if (title==null){
+            title=" ";
+        }
+        System.out.println(receive.length);
+        System.out.println("发送人编号"+empId);
+        System.out.println("发送的类别"+type);
+        System.out.println("发送人标题"+title);
+        System.out.println("发送人内容"+content);
         for (int receiveId : receive) {
            emailService.addEmail(empId,receiveId,title,content,type);
         }
@@ -65,21 +69,45 @@ public class EmailHandler {
      * @return
      */
     @GetMapping("findEmail")
-    public Result findEmailByReadStatusAndempId(int empId, int type, int readStatus, int receiveEmpId){
+    public Result findEmailByReadStatusAndempId(int emailId,int empId, int type, int readStatus, int receiveEmpId){
         Result result=new Result();
-        System.out.println("进入了");
-        System.out.println("接收到的员工id是"+empId);
-        System.out.println("接收到的类型是"+type);
-        System.out.println("接收到的状态是"+readStatus);
-        List<Email>list=emailService.findEmail(empId,type,readStatus,receiveEmpId);
+//        System.out.println("进入了");
+//        System.out.println("接收到的员工id是"+empId);
+//        System.out.println("接收到的类型是"+type);
+//        System.out.println("接收到的状态是"+readStatus);
+//        System.out.println("接收到的接收者id为"+receiveEmpId);
+        List<Email>list=emailService.findEmail(emailId,empId,type,readStatus,receiveEmpId);
         String[]Name=new String[list.size()];
         System.out.println("集合的长度是"+list.size());
-        for (int i=0;i<list.size();i++){
-            //获得接收人的对象
-            Emp emp=empService.findEmpById(list.get(i).getReceiveEmpId());
-            System.out.println("接收人的名字是"+emp.getEmpName());
-            Name[i]=emp.getEmpName();
+
+        if (type!=2){
+            if (emailId==0){
+                for (int i=0;i<list.size();i++){
+                    //获得接收人的对象
+                    Emp emp=empService.findEmpById(list.get(i).getReceiveEmpId());
+                    System.out.println("接收人的名字是"+emp.getEmpName());
+                    Name[i]=emp.getEmpName();
+                }
+            }else {
+                for (int i=0;i<list.size();i++){
+                    //获得接收人的对象
+                    Emp emp=empService.findEmpById(list.get(i).getEmpId());
+                    System.out.println("接收人的名字是"+emp.getEmpName());
+                    Name[i]=emp.getEmpName();
+                }
+                //修改阅读状态
+                emailService.updateReadStatusByEmailId(2,emailId);
+            }
+
+        }else {
+                for (int i=0;i<list.size();i++){
+                    //获得接收人的对象
+                    Emp emp=empService.findEmpById(list.get(i).getEmpId());
+                    System.out.println("接收人的名字是"+emp.getEmpName());
+                    Name[i]=emp.getEmpName();
+                }
         }
+
 
         result.setObject(Name);
         result.setList(list);
