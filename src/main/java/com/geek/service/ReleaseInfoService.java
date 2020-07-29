bff226ffe52c41666427f12aaf6cb10c7902b2a4
wj1031638743 <1031638743@qq.com>
2020-07-29 19:23:44 +0800

    20200729v1.4 信息公告查询handler

--- src/main/java/com/geek/service/ReleaseInfoService.java
+++ src/main/java/com/geek/service/ReleaseInfoService.java
@@ -5,17 +5,32 @@ import com.geek.pojo.ReleaseTabe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ReleaseInfoService {
     @Autowired
     private MessageDao messageDao;
 
+    /**
+     * 信息公告发布
+     * @param releaseTabe
+     * @return
+     */
     public boolean addInfo(ReleaseTabe releaseTabe)
     {
         int count = messageDao.addInfo(releaseTabe);
         return count>0;
     }
 
-
+    /**
+     * 查询所有信息公告(5条)
+     * @return
+     */
+    public List<ReleaseTabe> findReleaseInfo()
+    {
+        List<ReleaseTabe> releaseInfos = messageDao.findReleaseInfo();
+        return releaseInfos;
+    }
 
 }
